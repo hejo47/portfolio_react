96cf290acda1791220db2b433b8a5accdf37005d
hejo47 <hejo_47@naver.com>
2023-03-04 16:38:02 +0900

    230303 포트폴리오 리스트 작업(이미지 추가)

--- src/components/section/PortfolioList.js
+++ src/components/section/PortfolioList.js
@@ -2,56 +2,66 @@ import classes from "./PortfolioList.module.css";
 import Container from "../common/Container";
 import SectionTitle from "../common/SectionTitle";
 import PortfolioItem from "./PortfolioItem";
+import img1 from "../../img/kisa.png";
+import img2 from "../../img/bbatsue.png";
+import img3 from "../../img/kakao.png";
+import img4 from "../../img/r6.png";
+import img5 from "../../img/ikea.jpeg";
+import img6 from "../../img/kirsh.jpeg";
+import img7 from "../../img/subway.jpeg";
+import img8 from "../../img/gliding.jpeg";
+import img9 from "../../img/interpark.jpeg";
+import img10 from "../../img/pizza.jpeg";
 const portfoioList = [
   {
     text: "한국인터넷진흥원",
     id: 1,
-    img: "",
+    img: img1,
   },
   {
     text: "뺏슈",
     id: 2,
-    img: "",
+    img: img2,
   },
   {
     text: "카카오 뉴스",
     id: 10,
-    img: "",
+    img: img3,
   },
   {
     text: "레인보우 식스 시즈",
     id: 3,
-    img: "",
+    img: img4,
   },
   {
     text: "이케아",
     id: 4,
-    img: "",
+    img: img5,
   },
   {
     text: "키르시",
     id: 5,
-    img: "",
+    img: img6,
   },
   {
     text: "서브웨이",
     id: 6,
-    img: "",
+    img: img7,
   },
   {
     text: "글라이딩",
     id: 7,
-    img: "",
+    img: img8,
   },
   {
     text: "인터파크",
     id: 8,
-    img: "",
+    img: img9,
   },
   {
     text: "피자 하우스",
     id: 9,
-    img: "",
+    img: img10,
   },
 ];
 const PortfolioList = () => {
