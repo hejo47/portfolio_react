0212cd0485411e1e10e24437cc2576aebe67b99d
hejo47 <hejo_47@naver.com>
2023-03-17 14:16:48 +0900

    230314 포트폴리오 메인 작성 (반응형 제작 중)

--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
         <div className={classes.contactCon}>
           <p className={classes.contactText}>Let's Work Together!</p>
           <span className={classes.call}>Call: 010-4665-8883</span>
-          <span className={classes.email}>Email: andante0407@gamil.com</span>
+          <span className={classes.email}>Email: andante0407@gmail.com</span>
           <button className={classes.btn} onClick={() => window.open("mailto:andante0407@gmail.com")}>
             Contact
           </button>
