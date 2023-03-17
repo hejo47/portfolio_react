4e27cdd99eb1024442ea50912349242adaadd951
hejo47 <hejo_47@naver.com>
2023-03-17 13:56:16 +0900

    230314 포트폴리오 메인 작성 (타임라인 작성 중)

--- src/components/common/Timeline.js
+++ src/components/common/Timeline.js
@@ -38,21 +38,21 @@ const Timeline = ({ setObserver, callback }) => {
         </div>
         {/* <div className='message'>{message1}</div> */}
       </div>
-      <div id='timeline2' ref={timeline2} className='timeline' />
+      <div id='timeline2' ref={timeline2} className='timeline' style={{ height: "100vh" }} />
       <div className='circleWrapper'>
         <div id='circle2' ref={circle2} className='circle'>
           2
         </div>
         {/* <div className='message'>{message2}</div> */}
       </div>
-      <div id='timeline3' ref={timeline3} className='timeline' />
+      <div id='timeline3' ref={timeline3} className='timeline' style={{ height: "350vh" }} />
       <div className='circleWrapper'>
         <div id='circle3' ref={circle3} className='circle'>
           3
         </div>
         {/* <div className='message'>{message3}</div> */}
       </div>
-      <div id='timeline4' ref={timeline4} className='timeline' />
+      <div id='timeline4' ref={timeline4} className='timeline' style={{ height: "100vh" }} />
       <div className='circleWrapper'>
         <div id='circle4' ref={circle4} className='circle'>
           4
