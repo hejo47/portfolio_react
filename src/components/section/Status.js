604af357d3fa6cf19f8552695ae07755d4565a4e
hejo47 <hejo_47@naver.com>
2023-03-21 14:36:21 +0900

    230314 스테이터스 애니메이션 작성 중

--- src/components/section/Status.js
+++ src/components/section/Status.js
@@ -3,7 +3,7 @@ import Container from "../common/Container";
 import SectionTitle from "../common/SectionTitle";
 import classes from "./Status.module.css";
 import StatusBar from "./StatusBar";
-// import AOS from "aos";
+import AOS from "aos";
 const statusList = [
   {
     name: "HTML",
@@ -40,25 +40,25 @@ const statusList = [
 ];
 
 const Status = () => {
-  const [status, setstatus] = useState();
+  // const [status, setstatus] = useState();
 
-  const scrollHandler = () => {
-    setstatus(document.querySelector("#status").offsetTop);
-    console.log(status);
-  };
+  // const scrollHandler = () => {
+  //   setstatus(document.querySelector("#status").offsetTop);
+  //   console.log(status);
+  // };
   useEffect(() => {
-    // AOS.init();
+    AOS.init();
     window.addEventListener("scroll", function () {
-      scrollHandler();
-      if (window.scrollY >= status - 150) {
-      }
+      // scrollHandler();
+      // if (window.scrollY >= status - 150) {
+      // }
     });
   });
   return (
     <section className={classes.status} id='status'>
       <Container>
         <SectionTitle title='Status'></SectionTitle>
-        <div className={classes.statusCon}>
+        <div className={classes.statusCon} data-aos='fade-left' data-aos-delay='300'>
           {statusList.map((bar, idx) => {
             return <StatusBar key={idx} name={bar.name} percent={bar.percent}></StatusBar>;
           })}
