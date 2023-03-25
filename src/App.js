990a39946187d42636b9239e5c46b2259e5b6b2e
hejo47 <hejo_47@naver.com>
2023-03-25 16:08:45 +0900

    230314 포트폴리오 서브 수정

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ const portfoioList = [
     desc: "관공서 페이지, 반응형, 웹 표준",
     git: "https://github.com/hejo47/KISAweb",
     url: "https://hejo47.github.io/KISAweb/",
+    desc_d: "",
   },
   {
     text: "인터파크",
@@ -38,6 +39,7 @@ const portfoioList = [
     date: "7일",
     desc: "KAKAO OPEN API 사용",
     url: "https://hejoportfolio.netlify.app/project/interpark/main.html",
+    desc_d: "",
   },
   {
     text: "카카오 뉴스",
@@ -48,6 +50,7 @@ const portfoioList = [
     desc: "REACT 화면 구성",
     git: "https://github.com/hejo47/kakaoweb",
     url: "https://kakaoweb-ugrf-g2g9zaudf-hejo47.vercel.app/news",
+    desc_d: "",
   },
   {
     text: "레인보우 식스 시즈",
@@ -58,6 +61,7 @@ const portfoioList = [
     desc: "반응형, SCSS를 이용한 애니메이션",
     git: "https://github.com/hejo47/R6web",
     url: "https://hejo47.github.io/R6web/",
+    desc_d: "",
   },
   {
     text: "이케아",
@@ -67,6 +71,8 @@ const portfoioList = [
     date: "5일",
     desc: "",
     url: "https://hejoportfolio.netlify.app/project/ikea/index.html",
+    desc_d:
+      "브랜드 '이케아'의 홈페이지 디자인입니다. 표지의 슬라이드와 메뉴의 움직임은 JavaScript를 이용한 CSS 애니메이션으로 제작했습니다. JavaScript로 localStorage에 저장하는 방식을 사용해 로그인을 구현했고, 로그인에 성공할 시 메뉴에 로그아웃을 구현했습니다. 페이지 하단부에는 SNS 버튼과 시계를 배치했습니다.",
   },
   {
     text: "키르시",
@@ -76,6 +82,8 @@ const portfoioList = [
     date: "4일",
     desc: "제작 슬라이드",
     url: "https://hejoportfolio.netlify.app/project/kirsh/index.html",
+    desc_d:
+      "브랜드 '키르시'의 홈페이지 디자인입니다. 전체적으로 단순하고 깔끔한 느낌을 주기 위해 검정색과 흰색을 사용하여 디자인했습니다. 커버는 재밌고 트렌디한 분위기를 주기 위해서 비대칭 사선으로 내려 작업했고, 제품 레이아웃은 많이 반복되면 시각적으로 지루함을 느낄 수 있기 때문에 가로 슬라이드로 제작했습니다. 배너는 타이다이 패턴을 홍보하기 위해 만들었는데, 패턴이 주는 느낌이 신비, 레트로이기 때문에 옛날 느낌이 나게 제작했습니다.",
   },
   {
     text: "서브웨이",
@@ -85,6 +93,7 @@ const portfoioList = [
     date: "3일",
     desc: "반응형, SWIPER",
     url: "https://hejoportfolio.netlify.app/project/subway/index.html",
+    desc_d: "",
   },
   {
     text: "글라이딩",
@@ -94,6 +103,7 @@ const portfoioList = [
     date: "10일",
     desc: "반응형, 서브 페이지",
     url: "https://hejoportfolio.netlify.app/project/gliding/main.html",
+    desc_d: "",
   },
   {
     text: "뺏슈",
@@ -104,6 +114,7 @@ const portfoioList = [
     desc: "OPEN API",
     git: "https://github.com/hejo47/bbatsueWeb",
     url: "https://bbatsue-ar7vhp97x-hejo47.vercel.app/",
+    desc_d: "",
   },
   {
     text: "피자 하우스",
@@ -113,6 +124,7 @@ const portfoioList = [
     date: "8일",
     desc: "반응형",
     url: "https://hejoportfolio.netlify.app/project/pizza/main.html",
+    desc_d: "",
   },
 ];
 function App() {
--- src/components/pages/PortfolioDetail.js
+++ src/components/pages/PortfolioDetail.js
@@ -1,16 +1,32 @@
 import Container from "../common/Container";
 import classes from "./PortfolioDetail.module.css";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 const PortfolioDetail = (props) => {
   let { idx } = useParams();
   return (
     <div className={classes.detail}>
       <Container>
         <div className={classes.detailWrap}>
-          <h2 className={classes.detailTitle}>
-            {Number(idx) + 1}번째 프로젝트, {props.list[idx].text}
-          </h2>
           <img className={classes.detailimg} src={props.list[idx].img} alt={props.list[idx].text}></img>
+
+          <div className={classes.detailTextWrap}>
+            <h2 className={classes.detailTitle}>
+              {Number(idx) + 1}번째 프로젝트,
+              <br /> {props.list[idx].text}입니다.
+            </h2>
+            <div className={classes.textleft}>
+              <p>특징</p>
+              <span>{props.list[idx].desc_d}</span>
+              <Link to={props.list[idx].url} className={classes.btn}>
+                구현 화면
+              </Link>
+              {props.list[idx].git && (
+                <Link to={props.list[idx].git} className={classes.btn}>
+                  깃허브 주소
+                </Link>
+              )}
+            </div>
+          </div>
         </div>
         {/* {console.log(props.list[idx])} */}
       </Container>
